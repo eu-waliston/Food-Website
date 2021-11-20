82a4dcca7999fd78d541a6dca2c35c696b3d0c75
Waliston E. Santos <82295321+bkt-tonton@users.noreply.github.com>
2021-11-20 02:51:48 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const navbarToggler = document.querySelector('.navbar-toggler');
+navbarToggler.addEventListener('click', () => {
+  navbarToggler.classList.toggle('change-icon');
+  document.querySelector('.navbar-collapse').classList.toggle('show-navbar')
+});
