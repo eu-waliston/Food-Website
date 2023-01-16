90fa095a736fe81e5c270cb510c2443937da2b7c
walwizard <goneisgone@hotmail.com>
2023-01-16 11:50:35 -0300

    Atualização do projeto concluida!

---
+++ App.js
@@ -0,0 +1,20 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const path = require("path");
+const rootRouter = require("./src/routes/index");
+
+const app = express();
+
+app.use(express.json());
+app.use(urlencoded({extended: true}))
+
+app.use(express.static(__dirname + '/public'))
+
+app.set('views', path.join('./src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', rootRouter);
+
+app.listen(3000, () => {
+  console.log("App lisnten at port 3000!");
+});
--- js/script.js
+++
@@ -1,5 +0,0 @@
-const navbarToggler = document.querySelector('.navbar-toggler');
-navbarToggler.addEventListener('click', () => {
-  navbarToggler.classList.toggle('change-icon');
-  document.querySelector('.navbar-collapse').classList.toggle('show-navbar')
-});
